c47c6841f8931608278f284448a1a855b8d2fd9a
Jennifer Tu <jennifertu@gmail.com>
2017-09-30 01:00:08 -0700

    Add native file picker

--- index.android.js
+++ index.android.js
@@ -11,6 +11,21 @@ import {
   Text,
   View
 } from 'react-native';
+import {
+  DocumentPicker,
+  DocumentPickerUtil
+} from 'react-native-document-picker';
+
+DocumentPicker.show({
+    filetype: [DocumentPickerUtil.images()],
+  }, (error,res) => {
+    console.log(
+      res.uri,
+      res.type,
+      res.fileName,
+      res.fileSize
+    );
+});
 
 export default class GasLog extends Component {
   render() {
